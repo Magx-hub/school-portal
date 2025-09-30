de39985f793a2dff24cd979af9bd3a5aaefb3fa2
Magx-hub <mag4max@gmail.com>
2025-09-30 14:20:44 +0000

    Updates after PWA deployment

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   // GitHub Pages configuration
-  base: process.env.NODE_ENV === 'production' ? '/school-portal/' : '/',
+  base: '/',
   plugins: [
     react(),
     tailwindcss(),
